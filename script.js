5eb55ca9fedb60a6d9c028456848d6995731699c
meow <meow.noisy.code@gmail.com>
2021-06-11 19:52:16 +0900

    add function which shows hit and blow result

--- script.js
+++ script.js
@@ -10,9 +10,10 @@ function number_generator() {
     return selected;
 };
 
+var correct_array = number_generator()
+
 
 function count_num_of_hit_and_blow(estimate_array) {
-    correct_array = ["1", "2", "3", "4", "5"];
 
     let hit = 0;
     let blow = 0;
@@ -41,13 +42,10 @@ const predict = document.getElementById("predict");
 
 
 const reply_click = function () {
-    console.log("Button clicked, id " + this.id);
+    // console.log("Button clicked, id " + this.id);
     predict_num_list.push(this.id);
-
     predict.innerHTML = predict_num_list
-
 }
-
 
 
 
@@ -55,4 +53,15 @@ const btns = document.querySelectorAll(".btn");
 for (var i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", reply_click, false)
 }
+
+
+var result_text = document.getElementById("result");
+function submit_result() {
+    const result = count_num_of_hit_and_blow(predict_num_list);
+    result_text.innerHTML = `result: hit=${result["hit"]}, blow=${result["blow"]}`
+    console.log(result)
+}
+
+const submit_button = document.getElementById("submit_btn");
+submit_button.addEventListener("click", submit_result, false);
 
