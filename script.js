e11b93249c91cab2bceb223615e6a3228db92b16
meow <meow.noisy.code@gmail.com>
2021-06-12 14:25:05 +0900

    add clearing an array of predict, and when submitting array also clear

--- script.js
+++ script.js
@@ -55,13 +55,23 @@ for (var i = 0; i < btns.length; i++) {
 }
 
 
+function clear_result() {
+    predict_num_list = []
+    predict.innerHTML = "ボタンを押してね。"
+}
+
 var result_text = document.getElementById("result");
 function submit_result() {
     const result = count_num_of_hit_and_blow(predict_num_list);
     result_text.innerHTML = `result: hit=${result["hit"]}, blow=${result["blow"]}`
+    clear_result()
     console.log(result)
 }
+
+
 
 const submit_button = document.getElementById("submit_btn");
 submit_button.addEventListener("click", submit_result, false);
 
+var clear_btn = document.getElementById("clear_btn");
+clear_btn.addEventListener("click", clear_result, false);
